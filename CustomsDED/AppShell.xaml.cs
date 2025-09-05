abc4e92bd1ad4a688ce3e86e764891fbfd7d60e8
Rado <sitcom93@abv.bg>
2025-09-05 18:41:49 +0300

    Add project files.

---
+++ CustomsDED/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomsDED
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CustomsDED/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CustomsDED")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CustomsDED.Pages")]
