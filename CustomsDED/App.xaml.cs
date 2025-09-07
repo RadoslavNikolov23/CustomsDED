5af06e958ae5ef3c979d193539c1eb686cd8640c
Rado <sitcom93@abv.bg>
2025-09-08 01:48:09 +0300

    Add localization files and ajust some android option - screen lock and theme

--- CustomsDED/App.xaml.cs
+++ CustomsDED/App.xaml.cs
@@ -1,10 +1,20 @@
 ﻿namespace CustomsDED
 {
+    using Plugin.Maui.OCR;
+
+    using System.Globalization;
+
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            DependencyService.RegisterSingleton(OcrPlugin.Default);
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("bg-BG");
+
+            UserAppTheme = AppTheme.Light;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
